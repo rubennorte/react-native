70a73ca61d383d8a1766d88af9f0a6b63321caee
Rubén Norte <rubennorte@meta.com>
2026-09-10 04:18:03 -0700

    Make assorted remaining modules Flow strict-local (#57737)
    Summary:
    Pull Request resolved: https://github.com/react/react-native/pull/57737
    
    Upgrade the remaining assorted modules (Touchable pooling, ActionSheetIOS, Alert, LogBox highlight, Text native component, PushNotificationIOS) from `flow` to `flow strict-local`, preserving public type signatures so consumers are unaffected. Genuinely-dynamic internal `any` usages are marked with scoped `$FlowFixMe[unclear-type]` where an accurate type cannot be expressed.
    
    Changelog: [Internal]
    
    Reviewed By: javache
    
    Differential Revision: D113763783

--- packages/react-native/Libraries/LogBox/UI/AnsiHighlight.js
+++ packages/react-native/Libraries/LogBox/UI/AnsiHighlight.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow
+ * @flow strict-local
  * @format
  */
 
@@ -13,6 +13,7 @@ import type {TextStyleProp} from '../../StyleSheet/StyleSheet';
 import View from '../../Components/View/View';
 import StyleSheet from '../../StyleSheet/StyleSheet';
 import Text from '../../Text/Text';
+// $FlowFixMe[untyped-import]
 import {ansiToJson} from 'anser';
 import * as React from 'react';
 
