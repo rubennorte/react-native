3ae59df59c1a96380f77c3d40ded125b43f445a2
Rubén Norte <rubennorte@meta.com>
2026-09-10 04:10:03 -0700

    Make remaining ReactNative and Core modules Flow strict-local (#57730)
    Summary:
    Pull Request resolved: https://github.com/react/react-native/pull/57730
    
    Upgrade the remaining ReactNative and Core modules from `flow` to `flow strict-local`, preserving public type signatures so consumers are unaffected. Genuinely-dynamic internal `any` usages are marked with scoped `$FlowFixMe[unclear-type]` where an accurate type cannot be expressed.
    
    Changelog: [Internal]
    
    Reviewed By: javache
    
    Differential Revision: D113763791

--- packages/react-native/Libraries/Core/setUpReactRefresh.js
+++ packages/react-native/Libraries/Core/setUpReactRefresh.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow
+ * @flow strict-local
  * @format
  */
 
@@ -18,6 +18,7 @@ if (__DEV__) {
   }
 
   // This needs to run before the renderer initializes.
+  // $FlowFixMe[untyped-import]
   const ReactRefreshRuntime = require('react-refresh/runtime');
   ReactRefreshRuntime.injectIntoGlobalHook(global);
 
